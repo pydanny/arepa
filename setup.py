f963b2a5fc464f76e6d83efb084d3b64dd57090f
Daniel Roy Greenfeld <pydanny@gmail.com>
2018-03-09 21:19:35 -0500

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='arepa',
-    version='0.2.0',
+    version='0.2.1',
     description="Arepa-making utility",
     long_description=readme + '\n\n' + history,
     author="Daniel Roy Greenfeld",
